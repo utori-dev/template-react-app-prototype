64b4d50eb17ac258ff828051a0ced64f31262432
Michelle Miller <chellimiller@users.noreply.github.com>
2023-03-31 14:37:58 -0700

    Add templates and script to generate component

---
+++ .templates/generate.cjs
@@ -0,0 +1,104 @@
+const Mustache = require('mustache');
+const fs = require('fs/promises');
+const path = require('path');
+const { words, capitalize, kebabCase } = require('lodash');
+
+const PACKAGE_ROOT = path.dirname(__dirname);
+
+/**
+ * Converts a string value to PascalCase.
+ *
+ * @param {string} value
+ * @returns {string}
+ */
+function toPascalCase(value) {
+  return words(value).map(capitalize).join('');
+}
+
+/**
+ * Loads the specified template.
+ * Ignores sub-directories within the template.
+ *
+ * @param {string} name
+ * @returns {Promise<{ name: string, content: string }[]>}
+ */
+async function loadTemplates(name) {
+  const directory = path.join(__dirname, name);
+  const files = (
+    await fs.readdir(directory, {
+      withFileTypes: true,
+    })
+  ).filter((item) => item.isFile());
+
+  const templates = await Promise.all(
+    files.map((file) =>
+      fs
+        .readFile(path.join(directory, file.name), { encoding: 'utf-8' })
+        .then((content) => ({ name: file.name, content }))
+    )
+  );
+
+  return templates;
+}
+
+require('yargs')
+  .scriptName('generate')
+  .usage('$0 <cmd> [args]')
+  .command('component [name]', 'Generates a new UI component', {
+    builder: (yargs) => {
+      yargs.positional('name', {
+        describe: 'The name of the component.',
+        type: 'string',
+        require: true,
+        coerce: toPascalCase,
+      });
+      yargs.option('description', {
+        describe: 'Description of the component. Will be used for JSDocs.',
+        alias: 'd',
+        type: 'string',
+        default: '@todo Add description',
+        require: false,
+      });
+    },
+    handler: async (args) => {
+      const { name: namePascalCase, description } = args;
+      const nameKebabCase = kebabCase(namePascalCase);
+
+      const name = {
+        pascalCase: namePascalCase,
+        kebabCase: nameKebabCase,
+      };
+
+      const templates = await loadTemplates('Component');
+
+      const componentPath = path.join(
+        PACKAGE_ROOT,
+        'src',
+        'ui',
+        namePascalCase
+      );
+
+      await fs.mkdir(componentPath);
+
+      await Promise.all(
+        templates.map((template) =>
+          fs.writeFile(
+            path.join(
+              componentPath,
+              template.name
+                .replace('Component', namePascalCase)
+                .replace(/\.mustache$/, '')
+            ),
+            Mustache.render(template.content, { name, description })
+          )
+        )
+      );
+
+      console.log(`Successfully created ${namePascalCase}`);
+    },
+  })
+  .demandCommand(1, 'No command was specified')
+  .strictCommands()
+  .help()
+  .alias('h', 'help')
+  .alias('v', 'version').argv;
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,22 +2,20 @@ import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import App from './App';
 
-const snapshot = `
-<div
-  className="app"
->
-  <h1>
-    Hello World
-  </h1>
-</div>
-`;
-
 describe('App', () => {
   it('should match snapshot', () => {
     // Act
     const tree = renderer.create(<App />).toJSON();
 
     // Assert
-    expect(tree).toMatchInlineSnapshot(snapshot);
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="app"
+      >
+        <h1>
+          Hello World
+        </h1>
+      </div>
+    `);
   });
 });
