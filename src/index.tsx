d72c3b149a3372dd40fa7f9e710eee3340e9d82b
jared <jared.m.bourget@gmail.com>
2024-01-19 14:21:31 -0700

    Removing react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import store, { persistor } from './state/store/_store';
+import { store, persistor } from './state';
 
 // Ensure that the styles from index.css are included.
 import './index.css';
--- src/state/store/index.ts
+++ src/state/store/index.ts
@@ -1,3 +1,6 @@
 export * from './hooks';
 export * from './types';
 export * from './actions';
+
+export { default as store } from './_store';
+export * from './_store';
